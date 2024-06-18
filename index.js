484f49d80affb5740727fc1ecc0ab8214c782499
Ankit Dubey <106301985+ankitdevlop@users.noreply.github.com>
2024-06-19 00:26:10 +0530

    fix

--- index.js
+++ index.js
@@ -5,4 +5,40 @@ var typed = new Typed(".input", {
     loop: true
 })
 
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    // Show loader
+    document.getElementById('loader').classList.remove('hidden');
+
+    const formData = {
+      firstName: document.getElementById('first_name').value,
+      lastName: document.getElementById('last_name').value,
+      email: document.getElementById('email').value,
+      phone: document.getElementById('phone_number').value,
+      message: document.getElementById('message').value,
+    };
+
+    fetch('http://localhost:5000/send-email', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      // Hide loader
+      document.getElementById('loader').classList.add('hidden');
+      alert('Message sent successfully!');
+      // Clear form
+      document.getElementById('contact-form').reset();
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      // Hide loader
+      document.getElementById('loader').classList.add('hidden');
+      alert('There was an error sending your message.');
+    });
+  });
